8c342cfd4e7f8885a27f00d999ccf827fb60614d
[nataliac92] <[nataliacatube@gmail.com]>
2022-08-31 22:44:20 -0300

    cat

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,14 @@ import Image from 'next/image';
 import Layout from '../layout/Layout';
 import useQuiosco from '../hooks/useQuiosco';
 
-export default function Home({categorias}) {
+export default function Home() {
    
-  const { categoriaActual } = useQuiosco();
+  const {categoriaActual} = useQuiosco();
 
 
   return (
-     <Layout pagina={`Menú ${categoriaActual}`}>
-        <h1 className='text-4xl font-black'>{categoriaActual}</h1>
+     <Layout pagina={categoriaActual.nombre}>
+        <h1 className='text-4xl font-black'>{categoriaActual.nombre}</h1>
      </Layout>
   );
 }
